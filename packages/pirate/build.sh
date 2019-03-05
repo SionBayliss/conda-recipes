80f1be3f54e75b7ca7c92fd699c27d15b2103816
Ubuntu <s_bayliss@hotmail.co.uk>
2019-03-05 21:05:57 +0000

    updated pirate package for prerelease

--- packages/pirate/build.sh
+++ packages/pirate/build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-mkdir -p "${PREFIX}/bin" "${PREFIX}/test" "${PREFIX}/tools"
+mkdir -p "${PREFIX}/bin" "${PREFIX}/test" "${PREFIX}/tools" "${PREFIX}/scripts"
 mv bin/* "${PREFIX}/bin/"
 mv test/* "${PREFIX}/test/"
 mv tools/* "${PREFIX}/tools/"
-
+mv scripts/* "${PREFIX}/scripts/"
